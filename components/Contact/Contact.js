98a61921515c0887895350d2578bff5775e7e083
Pranav <jpranavniran@gmail.com>
2026-05-10 21:51:29 +0530

    navigation issue fix

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -263,7 +263,7 @@ const Contact = () => {
   return (
     <section
       ref={sectionRef}
-      id={MENULINKS[4].ref}
+      id={MENULINKS[5].ref}
       className="mt-30 w-full relative select-none bg-black pt-20 sm:pt-10 md:pt-5 lg:pt-1 pb-20"
     >
       <div>
